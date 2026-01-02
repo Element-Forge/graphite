51754db7930047b666bd0a24e831e6192b78087d
Philipp Gatzka <gatzka58@gmail.com>
2026-01-02 15:37:24 +0100

    #69 configure java 21 toolchain for all subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,9 @@
 plugins {
-    java
+    id("java")
 }
 
 group = property("group") as String
 version = property("version") as String
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(property("javaVersion") as String))
-    }
-}
-
-repositories {
-    mavenCentral()
-}
 
 allprojects {
     repositories {
@@ -23,6 +13,12 @@ allprojects {
 
 subprojects {
     apply(plugin = "java-library")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(property("javaVersion") as String))
+        }
+    }
 
     dependencies {
         "api"(rootProject.libs.jetbrains.annotations)
