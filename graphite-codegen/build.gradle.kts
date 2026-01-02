3389cdd36a5f26fcee49f64b96b78f796ba5c26f
Philipp Gatzka <gatzka58@gmail.com>
2026-01-02 23:41:01 +0100

    #54 add WireMock tests for SchemaIntrospector HTTP success path

--- graphite-codegen/build.gradle.kts
+++ graphite-codegen/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
     api(project(":graphite-core"))
     api(libs.graphql.java)
     api(libs.javapoet)
+    testImplementation(libs.wiremock)
 }
--- graphite-codegen/src/test/java/io/github/graphite/codegen/SchemaIntrospectorTest.java
+++ graphite-codegen/src/test/java/io/github/graphite/codegen/SchemaIntrospectorTest.java
@@ -1,14 +1,20 @@
 package io.github.graphite.codegen;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchemaIntrospectorTest {
@@ -145,5 +151,225 @@ class SchemaIntrospectorTest {
                 .build();
 
         assertThrows(Exception.class, introspector::fetchSchema);
+    }
+
+    @Test
+    void fetchSchemaSuccessfullyWithMockServer(@TempDir Path tempDir) throws Exception {
+        WireMockServer wireMock = new WireMockServer(0);
+        wireMock.start();
+
+        try {
+            String introspectionResponse = """
+                {
+                  "data": {
+                    "__schema": {
+                      "queryType": { "name": "Query" },
+                      "mutationType": null,
+                      "subscriptionType": null,
+                      "types": [
+                        {
+                          "kind": "OBJECT",
+                          "name": "Query",
+                          "description": null,
+                          "fields": [
+                            {
+                              "name": "hello",
+                              "description": null,
+                              "args": [],
+                              "type": { "kind": "SCALAR", "name": "String", "ofType": null },
+                              "isDeprecated": false,
+                              "deprecationReason": null
+                            }
+                          ],
+                          "inputFields": null,
+                          "interfaces": [],
+                          "enumValues": null,
+                          "possibleTypes": null
+                        },
+                        {
+                          "kind": "SCALAR",
+                          "name": "String",
+                          "description": null,
+                          "fields": null,
+                          "inputFields": null,
+                          "interfaces": null,
+                          "enumValues": null,
+                          "possibleTypes": null
+                        }
+                      ],
+                      "directives": []
+                    }
+                  }
+                }
+                """;
+
+            wireMock.stubFor(post(urlEqualTo("/graphql"))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody(introspectionResponse)));
+
+            SchemaIntrospector introspector = SchemaIntrospector.builder()
+                    .endpoint("http://localhost:" + wireMock.port() + "/graphql")
+                    .header("Authorization", "Bearer test-token")
+                    .build();
+
+            String schema = introspector.fetchSchema();
+
+            assertNotNull(schema);
+            assertTrue(schema.contains("Query"));
+        } finally {
+            wireMock.stop();
+        }
+    }
+
+    @Test
+    void fetchSchemaToFileSuccessfullyWithMockServer(@TempDir Path tempDir) throws Exception {
+        WireMockServer wireMock = new WireMockServer(0);
+        wireMock.start();
+
+        try {
+            String introspectionResponse = """
+                {
+                  "data": {
+                    "__schema": {
+                      "queryType": { "name": "Query" },
+                      "mutationType": null,
+                      "subscriptionType": null,
+                      "types": [
+                        {
+                          "kind": "OBJECT",
+                          "name": "Query",
+                          "description": null,
+                          "fields": [
+                            {
+                              "name": "hello",
+                              "description": null,
+                              "args": [],
+                              "type": { "kind": "SCALAR", "name": "String", "ofType": null },
+                              "isDeprecated": false,
+                              "deprecationReason": null
+                            }
+                          ],
+                          "inputFields": null,
+                          "interfaces": [],
+                          "enumValues": null,
+                          "possibleTypes": null
+                        },
+                        {
+                          "kind": "SCALAR",
+                          "name": "String",
+                          "description": null,
+                          "fields": null,
+                          "inputFields": null,
+                          "interfaces": null,
+                          "enumValues": null,
+                          "possibleTypes": null
+                        }
+                      ],
+                      "directives": []
+                    }
+                  }
+                }
+                """;
+
+            wireMock.stubFor(post(urlEqualTo("/graphql"))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody(introspectionResponse)));
+
+            Path outputPath = tempDir.resolve("schema.graphqls");
+
+            SchemaIntrospector introspector = SchemaIntrospector.builder()
+                    .endpoint("http://localhost:" + wireMock.port() + "/graphql")
+                    .build();
+
+            introspector.fetchSchemaToFile(outputPath);
+
+            assertTrue(Files.exists(outputPath));
+            String content = Files.readString(outputPath);
+            assertTrue(content.contains("Query"));
+        } finally {
+            wireMock.stop();
+        }
+    }
+
+    @Test
+    void fetchSchemaFailsWithNon200Response(@TempDir Path tempDir) throws Exception {
+        WireMockServer wireMock = new WireMockServer(0);
+        wireMock.start();
+
+        try {
+            wireMock.stubFor(post(urlEqualTo("/graphql"))
+                    .willReturn(aResponse()
+                            .withStatus(500)
+                            .withBody("Internal Server Error")));
+
+            SchemaIntrospector introspector = SchemaIntrospector.builder()
+                    .endpoint("http://localhost:" + wireMock.port() + "/graphql")
+                    .build();
+
+            IOException exception = assertThrows(IOException.class, introspector::fetchSchema);
+            assertTrue(exception.getMessage().contains("500"));
+        } finally {
+            wireMock.stop();
+        }
+    }
+
+    @Test
+    void fetchSchemaFailsWithGraphQLErrors(@TempDir Path tempDir) throws Exception {
+        WireMockServer wireMock = new WireMockServer(0);
+        wireMock.start();
+
+        try {
+            String errorResponse = """
+                {
+                  "errors": [
+                    { "message": "Not authorized" }
+                  ]
+                }
+                """;
+
+            wireMock.stubFor(post(urlEqualTo("/graphql"))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody(errorResponse)));
+
+            SchemaIntrospector introspector = SchemaIntrospector.builder()
+                    .endpoint("http://localhost:" + wireMock.port() + "/graphql")
+                    .build();
+
+            IOException exception = assertThrows(IOException.class, introspector::fetchSchema);
+            assertTrue(exception.getMessage().contains("errors"));
+        } finally {
+            wireMock.stop();
+        }
+    }
+
+    @Test
+    void fetchSchemaFailsWithMissingData(@TempDir Path tempDir) throws Exception {
+        WireMockServer wireMock = new WireMockServer(0);
+        wireMock.start();
+
+        try {
+            String emptyResponse = "{}";
+
+            wireMock.stubFor(post(urlEqualTo("/graphql"))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody(emptyResponse)));
+
+            SchemaIntrospector introspector = SchemaIntrospector.builder()
+                    .endpoint("http://localhost:" + wireMock.port() + "/graphql")
+                    .build();
+
+            IOException exception = assertThrows(IOException.class, introspector::fetchSchema);
+            assertTrue(exception.getMessage().contains("data"));
+        } finally {
+            wireMock.stop();
+        }
     }
 }
