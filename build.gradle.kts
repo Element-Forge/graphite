577c63cdf918d55398d779f0cda1cb9df5d3a62e
Philipp Gatzka <gatzka58@gmail.com>
2026-01-02 16:23:35 +0100

    #83 add ci pipeline with build, test, sonarcloud, and github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.sonarqube") version "5.1.0.4882"
 }
 
 group = property("group") as String
@@ -11,16 +12,75 @@ allprojects {
     }
 }
 
+sonar {
+    properties {
+        property("sonar.projectKey", "Element-Forge_graphite")
+        property("sonar.organization", "element-forge")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
 subprojects {
     apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+    apply(plugin = "jacoco")
 
     java {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(property("javaVersion") as String))
         }
+        withSourcesJar()
+        withJavadocJar()
     }
 
     dependencies {
         "api"(rootProject.libs.jetbrains.annotations)
+        "testImplementation"(rootProject.libs.junit.jupiter)
+    }
+
+    tasks.named<Test>("test") {
+        useJUnitPlatform()
+        finalizedBy(tasks.named("jacocoTestReport"))
+    }
+
+    tasks.named<JacocoReport>("jacocoTestReport") {
+        dependsOn(tasks.named("test"))
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+    }
+
+    tasks.named<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+        dependsOn(tasks.named("jacocoTestReport"))
+        violationRules {
+            rule {
+                limit {
+                    minimum = BigDecimal("0.95")
+                }
+            }
+        }
+    }
+
+    tasks.named("check") {
+        dependsOn(tasks.named("jacocoTestCoverageVerification"))
+    }
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/Element-Forge/graphite")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR") ?: ""
+                    password = System.getenv("GITHUB_TOKEN") ?: ""
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+        }
     }
 }
